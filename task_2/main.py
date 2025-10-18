0c3441ab5fba13ac3a0fd2b829c53ae7363e022f
alspezza <zspecian23@gmail.com>
2025-10-18 21:45:01 +0300

    1

--- task_2/main.py
+++ task_2/main.py
@@ -1,15 +1,28 @@
 #Специан Вариант1
-import json
+import json #Вводим модуль json
+exist = 0   #создаем переменную существования, начинаем код
 print("Start code: ")
-file = open(r"C:\Users\SpetsZak_88\Desktop\lr7\task_2\text.txt", "r", encoding="utf-8")
+#открываем текстовый файл с содержимым
+file = open(r"C:\Users\user\Desktop\Лабы и тп\ИПО\lr7\ipo-lr-7\text.txt" , "r", encoding="utf-8")
+#даем переменной content весь текст как список в пайтоне
 content = json.load(file)
-number = input("Введите номер квалификации: ")
+#вводим код профессии
+number = str(input("Введите номер квалификации: "))
+#проходимся по всем элементам файла
 for item in content:
-    if item.get("model") == "data.skill" and item.get("specialty") == number:
-        print("=============== Найдено ===============")
-        print(item["title"])
-        
-        
- 
-
+#цикл который проверяет есть ли в обьекте ключ с названием data.skill и присваиваем переменным нужные для нас значения
+    if item.get("model") == "data.skill": 
+        fields = item.get("fields", {})
+        code = fields.get("code")
+        title = fields.get("title")
+#если введенный код специальности совпадает, то выводим эту специальность
+        if code == number:
+            exist = 1
+            print("-"*20 +"Найдено"+ "-"*20)
+            print(f"{code} >> Название специальности: '{title}'")
+            print("-"*50)  
+#проверяем существует ли специальность через значение пременной exist и если ее нет то выводим ее отсутствие  
+if exist != 1:
+    print('-'*20+ "Файл не найден" + '-'*20)
+print("End code")
 file.close()
