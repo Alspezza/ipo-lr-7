15175b73ecccac114910d9f05b40addb7f50bd3a
alspezza <zspecian23@gmail.com>
2025-12-03 10:41:44 +0300

    promeshutochnoe

--- collision/main.py
+++ collision/main.py
@@ -33,5 +33,28 @@ def isCollisionRect(rect1, rect2):
         return True
 
 
+#print(isCollisionRect(rect1, rect2))
+
+
+def intersectionAreaRect(rect1,rect2):
+    [(r1x1, r1y1), (r1x2, r1y2)] = rect1 
+    [(r2x1, r2y1), (r2x2, r2y2)] = rect2 
+    if isCollisionRect(rect1,rect2) == False:
+        return 0 
+    elif isCollisionRect(rect1, rect2) == RectCorrectError:
+        raise ValueError('В функцию передан некорректный прямоугольник')
+    else:
+         x_left = max(x1_left, x2_left)
+    x_right = min(x1_right, x2_right)
+    
+    # Пересечение по оси Y
+    y_bottom = max(y1_bottom, y2_bottom)
+    y_top = min(y1_top, y2_top)
+    
+    # Вычисляем ширину и высоту пересечения
+    width = x_right - x_left
+    height = y_top - y_bottom
+
+
 print(isCollisionRect(rect1, rect2))
         
